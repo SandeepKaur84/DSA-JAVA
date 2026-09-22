03649afe9e608dde1b8f0c5f324acd40079dc27b
SandeepKaur84 <kaur190sandeep@gmail.com>
2026-09-22 17:49:45 +0530

    Strings basics done

---
+++ Strings/Basics.java
@@ -0,0 +1,25 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Basics {
+    public static void main(String[] args) {
+        String str = "Sandeep Kaur";
+        System.out.println(str);
+
+        //input + output
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String name = sc.nextLine();
+        System.out.println("Helloooo " + name);
+        sc.close();
+        
+        //length + concatenation
+        System.out.println("Length of your name is : " + name.length());
+
+        //charAt
+        for(int i = 0 ; i < name.length() ; i++) {
+            System.out.print(name.charAt(i));
+        }
+    }
+}
