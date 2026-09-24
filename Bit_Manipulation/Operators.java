976b2e9f891f4e027dc9a28e9dc3d2aacf534b71
SandeepKaur84 <kaur190sandeep@gmail.com>
2026-09-24 15:04:43 +0530

    Bitwise operators

---
+++ Bit_Manipulation/Operators.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Bitwise AND : " + (5 & 6));
+        System.out.println("Bitwise OR : " + (5 | 6));
+        System.out.println("Bitwise XOR : " + (5 ^ 6));
+        System.out.println("Bitwise One's complement : " + (~5));
+        System.out.println("Bitwise Left Shift : " + (5 << 2));
+        System.out.println("Bitwise Right Shift : " + (6 >> 1));
+    }
+}
